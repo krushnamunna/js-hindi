270c391dbe48ffa8d4219a9a6e196146855a8330
krushnamunna <66862803+krushnamunna@users.noreply.github.com>
2023-12-11 09:55:28 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "Radheshyam"
+let score = 34
+
+//console.log(typeof score);
+//console.log(typof(score));
+
+let valuInNumber = Number(score)
+//console.log(typof valueInNumber);
+//console.log(valueInNumber);
+
+// "34" => 34
+// "33af" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Radheshyam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ 
