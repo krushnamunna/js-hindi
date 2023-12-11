bcae7d3ad2ec47a980fa9ea1c140e74ea2746664
krushnamunna <66862803+krushnamunna@users.noreply.github.com>
2023-12-11 05:14:56 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+console.log(3
+    +
+    3) // code readability should be high
+    console.log("Radheshyam")
+
+    let name = "Radhe"
+    let age = 23
+    let isLoggedIn = false
+
+    //number => 2 yo power 53
+    //bigint  => long number use like stock market 
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined => 
+    // symbol => unique
+    // object
+
+    console.log(typeof "Radhe");
+    console.log(typeof null); // null object return and undefined is undefined return
+
